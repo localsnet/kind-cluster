ede01056e30c036750f00541c3b4c61ca5ec1500
localsnet <localsnet@example.com>
2023-09-19 18:06:17 -0600

    some commands excluded from sudo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kind-cluster
 local Kubernetes cluster in Docker containers for home/development/testing env. 
+
+To create cluster and it's config for correct user, you should specify username arg:
+```
+sudo ./install.sh $USER
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #local Kubernetes cluster in Docker containers for home/development/testing env.
+#Docker must be installed before run script.
+# User must be in Docker group
+# By positional parametr $1 we get the user name to create cluster and config deliberately to him.
 # Author: localsnet
 #Thanks to Laurent Roffe for his article https://medium.com/@laurent.roffe_24715/first-nextcloud-deployment-in-k8s-manual-full-steps-eca2566751e7
 arch="$(uname -m)"
@@ -18,10 +21,10 @@ sysctl fs.inotify.max_user_instances=4096
 #Create a cluster with three nodes
 # For AMD64 / x86_64
 if [[ $arch = x86_64 ]]; then
-  curl -Lo ./kind https://kind.sigs.k8s.io/dl/v$version/kind-linux-amd64
+sudo -u $1  curl -Lo ./kind https://kind.sigs.k8s.io/dl/v$version/kind-linux-amd64
 # For ARM64
 elif [[ $arch = aarch64 ]]; then
-  curl -Lo ./kind https://kind.sigs.k8s.io/dl/v$version/kind-linux-arm64
+ sudo -u $1 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v$version/kind-linux-arm64
 else
   printf "Your architecture $arch not supported"
   exit 256
@@ -29,4 +32,4 @@ fi
 
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
-kind create cluster --name $cluster_name --config cluster_3_nodes.yaml
+sudo -u $1 kind create cluster --name $cluster_name --config cluster_3_nodes.yaml
